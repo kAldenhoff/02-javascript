87c89709e6359efa06446e6a773523412dbfeb86
Kevin Aldenhoff <kevin.aldenhoff@gmail.com>
2018-11-06 12:26:16 +0100

    amelioration exercice 03-01

--- 03-colors/01-change-bcg-one/script.js
+++ 03-colors/01-change-bcg-one/script.js
@@ -12,21 +12,12 @@
 (function() {
 
     var color;
+    var allButtons = document.getElementsByTagName('button');
 
-    document.getElementById("red").addEventListener("click", function(){
-      document.body.style.backgroundColor="red";
-    });
-
-    document.getElementById("green").addEventListener("click", function(){
-      document.body.style.backgroundColor="green";
-    });
-
-    document.getElementById("yellow").addEventListener("click", function(){
-      document.body.style.backgroundColor="yellow";
-
-    document.getElementById("blue").addEventListener("click", function(){
-        document.body.style.backgroundColor="blue";
-      });
-    });
+    Array.from(allButtons).forEach(function(button){
+      button.addEventListener('click',function(){
+        document.body.style.backgroundColor = button.id;
+      })
+    })
 
 })();
